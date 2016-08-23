b4d3e2727c32c963add6776420f3871e86e8b191
Logan Buckley <logan.buckley@gmail.com>
2016-08-22 22:48:26 -0400

    Add support for chars, bools and nil.

--- driver.c
+++ driver.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+#define bool_f 0x2F
+#define bool_t 0x6F
+#define nil 0x3F
+
+#define fx_mask 0x03
+#define fx_tag 0x00
+#define fx_shift 2
+#define char_mask 0xFF
+#define char_tag 0x0F
+#define char_shift 8
+
+typedef unsigned int ptr;
+
+static void print_ptr(ptr x) {
+  if ((x & fx_mask) == fx_tag) {
+    printf("%d", ((int)x) >> fx_shift);
+  } else if (x == bool_f) {
+    printf("#f");
+  } else if (x == bool_t) {
+    printf("#t");
+  } else if (x == nil) {
+    printf("()");
+  } else if ((x & char_mask) == char_tag) {
+    printf("%c", ((int)x) >> char_shift);
+  } else {
+    printf("#<unknown 0x%08x", x);
+  }
+  printf("\n");
+}
+
 int main() {
-  printf("%d\n", scheme_entry());
+  print_ptr(scheme_entry());
   return 0;
 }
--- giles.scm
+++ giles.scm
@@ -1,17 +1,31 @@
+(define output-file (open-file "scheme_entry.s" "w"))
+
+(define (emit . format-args)
+  (define (format-string . args)
+    (apply simple-format (cons #f args)))
+  (let ((output (apply format-string format-args)))
+    (display output)
+    (display output output-file)))
+
 (define (compile-program x)
-  (define output-file (open-file "scheme_entry.s" "w"))
-  (define (emit . format-args)
-    (define (format-string . args)
-      (apply simple-format (cons #f args)))
-    (let ((output (apply format-string format-args)))
-      (display output)
-      (display output output-file)))
+  (define fixnum-shift 2)
+  (define (immediate-rep x)
+    (cond
+     ;; Fixnums are encoded with tag b00
+     ((integer? x) (ash x fixnum-shift))
+     ((eq? #f x) #b00101111)
+     ((eq? #t x) #b01101111)
+     ((null? x) #b00111111)
+     ;; #b1111 is the tag value for immediate types besides fixnums
+     ((char? x) (+ #b1111 (ash (char->integer x) 8)))))
   (emit "\t.text\n")
   (emit "\t.globl scheme_entry\n")
   (emit "scheme_entry:\n")
-  (emit "\tmovl $~a, %eax\n" x)
-  (emit "\tret\n")
-  (force-output)
-  (close-port output-file))
+  (emit "\tmovl $~a, %eax\n" (immediate-rep x))
+  (emit "\tret\n"))
 
-(compile-program 42)
+(with-output-to-port output-file
+  (lambda ()
+    (compile-program #\A)
+    (force-output)
+    (close-port output-file)))
